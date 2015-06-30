eade50260922c17bed5b3decfb3a41ea6e6f1be5
Israel Salazar <josemvp16@gmail.com>
2015-06-29 19:51:12 -0500

    DevelopShift library update
    examples updated

--- DevelopShift/src/TestLibraries/RunFile.java
+++ DevelopShift/src/TestLibraries/RunFile.java
@@ -2,6 +2,8 @@
 package TestLibraries;
 
 import Splash.SplashPanel;
+import java.awt.Color;
+import java.awt.Rectangle;
 
 
 public class RunFile {
@@ -9,6 +11,11 @@ public class RunFile {
     public static void main(String[] args) {
         LoginExample loginExample = new LoginExample();
         SplashPanel splash = new SplashPanel();
+        splash.setImageURL("/Images/Logo.png");
+        splash.setColorBar(new Color(215,215,215));
+        splash.setProgressBar(new Rectangle(0,0,600,15));
+        splash.setTimer(300,10);
+        splash.setProgressSpeed(2);
         splash.startSplash(loginExample);
     }
     
